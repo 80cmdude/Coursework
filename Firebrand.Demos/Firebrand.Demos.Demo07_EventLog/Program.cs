eb9c3e8357b7b5cb3c713851d46d1b4d8c62a881
80cmdude <s80cmdude@gmail.com>
2016-09-26 06:40:00 -0700

    .
    .

---
+++ Firebrand.Demos/Firebrand.Demos.Demo07_EventLog/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos.Demo07_EventLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void WriteEvent(string message)
+        {
+            string ApplicationName = "Facebook";
+        }
+    }
+}
