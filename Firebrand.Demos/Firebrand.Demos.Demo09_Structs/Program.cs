52e599f313fa2420e1a637a332077b3227e15b82
80cmdude <s80cmdude@gmail.com>
2016-09-26 08:44:14 -0700

    Structs
    Scrtucts are objects that only live on the stack

---
+++ Firebrand.Demos/Firebrand.Demos.Demo09_Structs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Longitude");
+            int lng = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Latitude");
+            int lat = Convert.ToInt32(Console.ReadLine());
+
+            Coordinates MyCoordinates = new Coordinates();
+            MyCoordinates.latitude = lat;
+            MyCoordinates.longitude = lng;
+
+            Console.WriteLine("Latitude is: " + MyCoordinates.latitude);
+            Console.WriteLine("Longitude is: " + MyCoordinates.longitude);
+
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+        }
+    }
+
+    public struct Coordinates
+    {
+        private int _latitude, _longitude;
+
+        public int latitude
+        {
+            get
+            {
+                return _latitude;
+            }
+            set
+            {
+                _latitude = value;
+            }
+        }
+
+        public int longitude
+        {
+            get
+            {
+                return _longitude;
+            }
+            set
+            {
+                _longitude = value;
+            }
+        }
+
+    }
+}
