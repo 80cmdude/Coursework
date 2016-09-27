cde1b015145b720fd1441a9cb6520ea4c3d080a0
80cmdude <s80cmdude@gmail.com>
2016-09-27 00:41:37 -0700

    Unit Test
    Very Simple first unit test

---
+++ Firebrand.Demos/Firebrand.Demos.Demo11_UnitTest/SimpleCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Firebrand.Demos
+{
+    [TestClass]
+    public class SimpleCalculatorTest
+    {
+        [TestMethod]
+        public void SubTest()
+        {
+            //Arrange 
+            int actual, expected;
+            expected = 25;
+            
+            SimpleCalculator calc = new SimpleCalculator();
+
+            //Act
+            actual = calc.Sub(35, 10);
+
+            //Assert
+            Assert.AreEqual(actual, expected, "Expected and actual values do not match");
+
+        }
+    }
+}
