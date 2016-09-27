3f5d42a9cb147e9acf882513a685523fac921e80
80cmdude <s80cmdude@gmail.com>
2016-09-27 07:29:59 -0700

    refresh commit

---
+++ Firebrand.Demos/Firebrand.Demos.Demo14_ExtensionMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos.Demo14_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Is {0} a valid email? {1}", email, email.IsValidEmail());
+            Console.ReadKey();
+        }
+    }
+
+    static class MyFunctions 
+    {
+        public static bool IsValidEmail(this string email)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        }
+    }
+}
