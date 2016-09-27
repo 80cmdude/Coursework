1bf655b81d6547adc6bb3ba057306d3eda160d26
80cmdude <s80cmdude@gmail.com>
2016-09-27 10:16:45 -0700

    Entitty Framework

---
+++ Firebrand.Demos/Firebrand.Demos.Demo17_EnitityFramework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos.Demo17_EnitityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new FourthCoffeeEntities();
+
+            var employees = context.Employees;
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0} this person was born in {1}", employee.FirstName, employee.DateOfBirth);
+            }
+
+            var emp = context.Employees.First(x => x.FirstName == "Charlie");
+
+            Console.WriteLine("We found {0}, {1}", emp.LastName, emp.FirstName);
+            emp.LastName = "Smith";
+            context.SaveChanges();
+
+            Console.ReadKey();
+        }
+    }
+}
