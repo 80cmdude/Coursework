9947c7c8161e68f98589c7dcda23bfa95f0b2062
80cmdude <s80cmdude@gmail.com>
2016-09-28 07:27:02 -0700

    Async

--- Firebrand.Demos/Firebrand.Demos.Demo22_ParallelTasks/Program.cs
+++ Firebrand.Demos/Firebrand.Demos.Demo22_ParallelTasks/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Firebrand.Demos.Demo22_ParallelTasks
@@ -10,6 +11,25 @@ namespace Firebrand.Demos.Demo22_ParallelTasks
     {
         static void Main(string[] args)
         {
+            var t1 = new Task(() => ExecuteLongRunningJob(1, 3000));
+            t1.Start();
+            var t2 = new Task(() => ExecuteLongRunningJob(2, 4000));
+            t2.Start();
+            var t3 = new Task(() => ExecuteLongRunningJob(3, 2000));
+            t3.Start();
+
+            Task.WaitAll(t1, t2, t3);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static void ExecuteLongRunningJob(int id, int SleepTime)
+        {
+            Console.WriteLine("Task {0} is begining", id);
+            Thread.Sleep(SleepTime);
+            Console.WriteLine("Task {0} is ended", id);
+
         }
     }
 }
---
+++ Firebrand.Demos/Firebrand.Demos.Demo23_AsyncAwait/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Firebrand.Demos.Demo23_AsyncAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() => ExecuteLongRunningJob("Sam"));
+
+            lblMessage.Content = ExecuteLongRunningJob("Sam");
+        }
+
+        private void UpdateButton(string name)
+        {
+            var result = ExecuteLongRunningJob(name);
+            lblMessage.Content = result;
+        }
+
+        private string ExecuteLongRunningJob(string name)
+        {
+            Thread.Sleep(10000);
+            return "Hello, " + name;
+        }
+    }
+}
