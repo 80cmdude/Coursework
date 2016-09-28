a393f1ebc71d1b0bade5cd1503d36237a08da335
80cmdude <s80cmdude@gmail.com>
2016-09-28 06:03:39 -0700

    Delegates

---
+++ Firebrand.Demos/Firebrand.Demos.Demo21_Delegates/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firebrand.Demos.Demo21_Delegates
+{
+    class Program
+    {
+        //Create class called MyDelegate
+        delegate void MyDelegate(string name);
+
+        static void Main(string[] args)
+        {
+            SayHello("Sam");
+
+            //Get a hold of a reference to a method
+            MyDelegate del = new MyDelegate(SayGoodbye);
+            del.Invoke("Ash");
+            del("Orochi");
+            Test(del);
+
+            MyDelegate del1 = delegate(string name) { Console.WriteLine("Hey There!, {0}", name); };
+            del1("Anonomous Delegate");
+
+            //lamda expressions
+
+
+            Console.ReadKey();
+        }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine("Hey There!, {0}", name);
+        }
+
+        static void SayGoodbye(string name)
+        {
+            Console.WriteLine("Goodbye, {0}", name);
+        }
+
+        static void Test(MyDelegate del)
+        {
+            del("Desu");
+        }
+    }
+}
